143b8d652eed906732a99e1127ab9bd29979d130
VasoBolkvadze <vaso.bolkvadze@gmail.com>
2014-06-29 23:49:03 +0400

    uf

--- application/config/routes.js
+++ application/config/routes.js
@@ -9,19 +9,10 @@ module.exports.init = function(app){
 	router.get('/licenziebi',controllers.licenziebi.sia);
 	router.get('/licenziebi/axali',controllers.licenziebi.axali);
 	router.post('/licenziebi/axali',controllers.licenziebi.save);
-	router.get('/templates/:tpl',function(req,res){
-		res.render(req.params.tpl
-			, {}
-			, function(err,html) {
-				console.log('html',html);
-				res.send(html);
-			});
-	});
 	router.get('/templates/:subdir/:tpl',function(req,res){
 		res.render(req.params.subdir + '/' + req.params.tpl
 			, {}
 			, function(err,html) {
-				console.log('html',html);
 				res.send(html);
 			});
 	});
